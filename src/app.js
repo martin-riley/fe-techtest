2072cf713387c090676ca4914c3737c69f263dd8
Martin Riley <martin.m.riley@gmail.com>
2020-11-25 14:36:04 +0000

    Add functionality

--- src/app.js
+++ src/app.js
@@ -1,6 +1,34 @@
 // source goes here
-const app = function() {
-  console.log('hello from app!');
-};
+const app = () => {
+  let noOfButtons = 0;
+  let multiple = 0;
+
+  const createNumberButtons = (noOfButtons, multiple) => {
+    const NumberArray = [...Array(noOfButtons + 1).keys()]; // adding 1 to get from 0 to full number
+    const buttons = NumberArray.map((number) => {
+      const isMultiple = number % multiple === 0 ? ' button__multiple' : '';
+
+      if (number !== 0) {
+        return `<button class="button${isMultiple}">${number}</button>`;
+      }
+    });
+    return buttons.join('');
+  }
+
+  const updateButtons = (e) => {
+    multiple = parseInt(e.target.innerHTML);
+    root.innerHTML = createNumberButtons(noOfButtons, multiple);
+  }
+
+  const init = () => {
+    noOfButtons = 144;
+    document.body.innerHTML += '<div id="root" class="container"></div>';
+    const root = document.querySelector('#root');
+    root.innerHTML = createNumberButtons(noOfButtons, multiple);
+    root.addEventListener('click', updateButtons, false);
+  }
+
+  init(144);
+}
 
 export default app;
